cfa91ba83173b2e854679285b5b8456a35687a83
antonio <antoniomanzari@gmail.com>
2021-02-28 21:19:51 -0500

    added category menu view component

--- PluralsightPieCourse/Controllers/PieController.cs
+++ PluralsightPieCourse/Controllers/PieController.cs
@@ -18,7 +18,8 @@ namespace PluralsightPieCourse.Controllers
             _pieRepository = pieRepository;
             _categoryRepository = categoryRepository;
         }
-
+        
+        /*
         public ViewResult List()
         {
             PiesListViewModel piesListViewModel = new PiesListViewModel();
@@ -26,7 +27,7 @@ namespace PluralsightPieCourse.Controllers
             piesListViewModel.CurrentCategory = "Cheese Cakes";
 
             return View(piesListViewModel);
-        }
+        }*/
 
         public IActionResult Details(int id)
         {
@@ -38,6 +39,30 @@ namespace PluralsightPieCourse.Controllers
             }
 
             return View(pie);
+        }
+
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });   
         }
     }
 }
